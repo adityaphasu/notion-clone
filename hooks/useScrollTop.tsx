f2c66390d7954566281b0a7922beae6c42004843
Aditya Phasu <adityaphasu919@gmail.com>
2024-01-26 21:01:09 +0530

    Add mode toggle component, theme provider, and useScrollTop hook

---
+++ hooks/useScrollTop.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export const useScrollTop = (treshold = 10) => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > treshold) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [treshold]);
+
+  return scrolled;
+};
