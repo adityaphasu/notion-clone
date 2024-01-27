eea3af3ca8dec0bf92c6d8925091d1ce195b001b
Aditya Phasu <adityaphasu919@gmail.com>
2024-01-27 23:07:19 +0530

    Add DocumentsPage and MainLayout components

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
+const DocumentsPage = () => {
+  const { user } = useUser();
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        alt="empty"
+        height="300"
+        width="300"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        alt="empty"
+        height="300"
+        width="300"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button>
+        <PlusCircle className="mr-2 h-4 w-4" />
+        Create a note
+      </Button>
+    </div>
+  );
+};
+export default DocumentsPage;
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Spinner } from "@/components/spinner";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+import Navigation from "./_components/Navigation";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <Spinner size="md" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="flex h-full dark:bg-[#1F1F1F]">
+      <Navigation />
+      <main className="h-full flex-1 overflow-y-auto">{children}</main>
+    </div>
+  );
+};
+export default MainLayout;
