baab233614f32eb3fccf8c3072c9fa6ae7553d3a
Aditya Phasu <adityaphasu919@gmail.com>
2024-01-31 20:42:06 +0530

    Add search command component to main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
 import Navigation from "./_components/Navigation";
+import { SearchCommand } from "@/components/search-command";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -23,7 +24,10 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-full dark:bg-[#1F1F1F]">
       <Navigation />
-      <main className="h-full flex-1 overflow-y-auto">{children}</main>
+      <main className="h-full flex-1 overflow-y-auto">
+        <SearchCommand />
+        {children}
+      </main>
     </div>
   );
 };
