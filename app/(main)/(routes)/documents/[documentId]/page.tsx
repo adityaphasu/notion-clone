a6d6227888c8add9a3421066ae7d953ae7a83797
Aditya Phasu <adityaphasu919@gmail.com>
2024-02-02 22:20:57 +0530

    Refactor document page and button component

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,4 +1,36 @@
-const DocumentIdPage = () => {
-  return <div>DocumentIdPage</div>;
+"use client";
+
+import { Toolbar } from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <div>Loading....</div>;
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]"></div>
+      <div className="mx-auto md:max-w-3xl lg:max-w-4xl">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 };
 export default DocumentIdPage;
