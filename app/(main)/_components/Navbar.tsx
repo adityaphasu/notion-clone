5ccd54a8fd440f7e81d440a0033b613e4926a2df
Aditya Phasu <adityaphasu919@gmail.com>
2024-02-02 22:20:51 +0530

    Add Banner and Menu components to Navbar

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./Title";
+import { Banner } from "./Banner";
+import { Menu } from "./Menu";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -20,8 +22,11 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   if (document === undefined) {
     return (
-      <nav className="w-full items-center bg-background px-3 py-2 dark:bg-[#1F1F1F]">
+      <nav className="flex w-full items-center justify-between bg-background px-3 py-2 dark:bg-[#1F1F1F]">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2 ">
+          <Menu.Skeleton />
+        </div>
       </nav>
     );
   }
@@ -43,8 +48,12 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         )}
         <div className="flex w-full items-center justify-between">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   );
 };
