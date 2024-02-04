47dc401803916f53e74bf244e9047c446ed2624e
Aditya Phasu <adityaphasu919@gmail.com>
2024-02-04 20:06:11 +0530

    Add beforeDelete() method to publicFiles in edgeStoreRouter

--- app/api/edgestore/[...edgestore]/route.ts
+++ app/api/edgestore/[...edgestore]/route.ts
@@ -5,7 +5,9 @@ const es = initEdgeStore.create();
  * This is the main router for the Edge Store buckets.
  */
 const edgeStoreRouter = es.router({
-  publicFiles: es.fileBucket(),
+  publicFiles: es.fileBucket().beforeDelete(() => {
+    return true;
+  }),
 });
 const handler = createEdgeStoreNextHandler({
   router: edgeStoreRouter,
