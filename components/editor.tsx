e1bb6aad52f4997457733a7084b6d3e563354a95
Aditya Phasu <adityaphasu919@gmail.com>
2024-02-05 22:07:46 +0530

    Add Editor component

---
+++ components/editor.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteView, useBlockNote, DragHandleMenu } from "@blocknote/react";
+import "@blocknote/core/style.css";
+import { useTheme } from "next-themes";
+import { useEdgeStore } from "@/lib/edgestore";
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+}
+
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+  const { resolvedTheme } = useTheme();
+
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file: File) => {
+    const res = await edgestore.publicFiles.upload({
+      file,
+    });
+
+    return res.url;
+  };
+
+  const editor: BlockNoteEditor = useBlockNote({
+    editable,
+    initialContent: initialContent
+      ? (JSON.parse(initialContent) as PartialBlock[])
+      : undefined,
+    onEditorContentChange: (editor) => {
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
+    },
+    uploadFile: handleUpload,
+  });
+
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
+      ></BlockNoteView>
+    </div>
+  );
+};
+
+export default Editor;
