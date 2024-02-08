6fc4f62eb1aab71cffb20d87e8fd847f84d97c35
Aditya Phasu <adityaphasu919@gmail.com>
2024-02-08 21:46:29 +0530

    Update Heading component styling and content

--- app/(landing)/_components/Heading.tsx
+++ app/(landing)/_components/Heading.tsx
@@ -12,11 +12,11 @@ export const Heading = () => {
 
   return (
     <div className="max-w-3xl space-y-4">
-      <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">
-        Your Ideas, Documents, & Plans. Welcome to{" "}
+      <h1 className="text-3xl font-bold sm:text-5xl md:text-5xl">
+        Your Ideas💡, Documents📕, & Plans🚀. Welcome to{" "}
         <span className="underline">Zotion</span>
       </h1>
-      <h3 className="text-base font-medium sm:text-xl md:text-2xl">
+      <h3 className="text-base font-medium sm:text-xl">
         Zotion is the connected workspace where <br /> better, faster work
         happens.
       </h3>
