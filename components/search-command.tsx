66399896e836fdf7c88a4ef8b52b1762b4754950
Aditya Phasu <adityaphasu919@gmail.com>
2024-09-18 05:28:29 +0530

    refactor: set CommandItem value to document title for improved search relevance

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -61,7 +61,7 @@ export const SearchCommand = () => {
           {documents?.map((document) => (
             <CommandItem
               key={document._id}
-              value={`${document._id}-${document.title}`}
+              value={document.title}
               title={document.title}
               onSelect={() => onSelect(document._id)}
             >
