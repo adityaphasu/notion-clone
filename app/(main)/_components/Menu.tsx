25e960a2ef5f826e82efad5e942a1c9998c11ae2
Aditya Phasu <adityaphasu919@gmail.com>
2026-05-20 22:02:04 +0530

    add small text and full width toggles to menu component

--- app/(main)/_components/Menu.tsx
+++ app/(main)/_components/Menu.tsx
@@ -12,12 +12,19 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { MoreHorizontal, Settings, Trash } from "lucide-react";
+import {
+  AArrowDown,
+  Maximize2,
+  MoreHorizontal,
+  Settings,
+  Trash,
+} from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useSettings } from "@/hooks/useSettingsModal";
 import { ActionTooltip } from "@/components/action-tooltip";
 import { useWordCount } from "@/hooks/useWordCount";
+import { Switch } from "@/components/ui/switch";
 
 interface MenuProps {
   documentId: Id<"documents">;
@@ -29,10 +36,15 @@ export const Menu = ({ documentId }: MenuProps) => {
   const settings = useSettings();
   const words = useWordCount();
 
-  const archive = useMutation(api.documents.archive);
   const document = useQuery(api.documents.getById, {
     documentId,
   });
+  const archive = useMutation(api.documents.archive);
+  const update = useMutation(api.documents.update);
+
+  const isFullWidth = document?.fullWidth ?? true;
+  const isSmallText = !!document?.smallText;
+  console.log(document?.fullWidth);
 
   const onArchive = () => {
     router.push("/documents");
@@ -42,6 +54,20 @@ export const Menu = ({ documentId }: MenuProps) => {
       loading: "Moving to trash...",
       success: "Note moved to trash!",
       error: "Failed to archive note.",
+    });
+  };
+
+  const onFullWidthChange = (checked: boolean) => {
+    update({
+      id: documentId,
+      fullWidth: checked,
+    });
+  };
+
+  const onSmallTextChange = (checked: boolean) => {
+    update({
+      id: documentId,
+      smallText: checked,
     });
   };
 
@@ -55,11 +81,46 @@ export const Menu = ({ documentId }: MenuProps) => {
         </DropdownMenuTrigger>
       </ActionTooltip>
       <DropdownMenuContent
-        className="w-65"
+        className="w-65 px-2"
         align="end"
         alignOffset={8}
         forceMount
       >
+        <DropdownMenuItem
+          className="flex items-center justify-between"
+          onSelect={(e) => e.preventDefault()}
+          onClick={() => {
+            onSmallTextChange(!isSmallText);
+          }}
+        >
+          <div className="flex items-center justify-between gap-1">
+            <AArrowDown className="mr-2 h-4 w-4" />
+            Small text
+          </div>
+          <Switch
+            size="sm"
+            checked={isSmallText}
+            onCheckedChange={onSmallTextChange}
+          />
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onSelect={(e) => e.preventDefault()}
+          onClick={() => {
+            onFullWidthChange(!isFullWidth);
+          }}
+          className="flex items-center justify-between"
+        >
+          <div className="flex items-center justify-between gap-1">
+            <Maximize2 className="mr-2 h-4 w-4 rotate-45" />
+            Full width
+          </div>
+          <Switch
+            size="sm"
+            checked={isFullWidth}
+            onCheckedChange={onFullWidthChange}
+          />
+        </DropdownMenuItem>
+        <DropdownMenuSeparator className="h-px" />
         <DropdownMenuItem onClick={settings.onOpen}>
           <Settings className="mr-2 h-4 w-4" />
           Settings
@@ -68,7 +129,7 @@ export const Menu = ({ documentId }: MenuProps) => {
           <Trash className="mr-2 h-4 w-4" />
           Move to Trash
         </DropdownMenuItem>
-        <DropdownMenuSeparator />
+        <DropdownMenuSeparator className="text-gray-500" />
         <div className="text-muted-foreground/70 space-y-0.5 p-2 text-[.6875rem]">
           <p>
             Word count: {words.wordCount}{" "}
